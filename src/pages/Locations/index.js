b2c1bc808f88af2ad94111b089050f107754ba11
Navneet Vishwakarma <navneetk7048@gmail.com>
2022-05-25 14:31:50 +0530

    Added Locations Page

--- src/pages/Locations/index.js
+++ src/pages/Locations/index.js
@@ -1,15 +1,44 @@
+import { FaMapMarkerAlt, FaPhoneAlt } from "react-icons/fa";
+
 import Banner from "../../components/Banner";
+import Button from "../../components/Button";
+import { locations } from "./locations";
 import locationBanner from "../../assets/images/location-banner.jpg";
 
 const Locations = () => {
   return (
-    <div className="">
+    <div className="locations ">
       <Banner
         banner={locationBanner}
         previous="Home"
         current="Locations"
         title="Locations"
       />
+      <div className="location-list container">
+        <h1>Healthcare Locations</h1>
+        {locations.map(({ id, name, address, contact, iframe, link }) => (
+          <>
+            <hr className="location-separator" />
+            <div className="location" key={id}>
+              <div>
+                <h2>{name}</h2>
+                <p className="location-address">
+                  <FaMapMarkerAlt /> {address}
+                </p>
+                <a href={`tel:${contact}`} className="location-contact">
+                  <FaPhoneAlt /> {contact}
+                </a>
+                <Button
+                  type="btn-pink-darkblue"
+                  text="Get Directions"
+                  onClick={() => window.open(link, "_blank")}
+                />
+              </div>
+              {iframe}
+            </div>
+          </>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/pages/Locations/locations.js
@@ -0,0 +1,66 @@
+export const locations = [
+  {
+    id: 1,
+    name: "Healthcare Medical Center",
+    address: "3947 Terry Lane, Winter Park, Florida, 32789",
+    contact: "(561) 223-1234",
+    iframe: (
+      <iframe
+        title="Healthcare Medical Center"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d56047.81003023557!2d-81.35725!3d28.600133!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdbdbe7727fa51f27!2sAdventHealth%20Winter%20Park!5e0!3m2!1sen!2sin!4v1653461568986!5m2!1sen!2sin"
+        style={{ border: "0" }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+    ),
+    link: "https://goo.gl/maps/hkK2yaJX3SDbsJhb6",
+  },
+  {
+    id: 2,
+    name: "Alego Health Co.",
+    address: "3007 Cotter Lake Cir, Missouri City, TX, 77459",
+    contact: "(912) 523-5983",
+    iframe: (
+      <iframe
+        title="Alego Health Co."
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6940.1228521680705!2d-95.576763!3d29.572816000000003!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640e5e410135fbb%3A0x22f88f179471f996!2s3007%20Cotter%20Lake%20Cir%2C%20Missouri%20City%2C%20TX%2077459%2C%20USA!5e0!3m2!1sen!2sin!4v1653462982885!5m2!1sen!2sin"
+        style={{ border: "0" }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+    ),
+    link: "https://goo.gl/maps/XmVjDHDbytX9bQHW9",
+  },
+  {
+    id: 3,
+    name: "Echo Health Center",
+    address: "1950 Lacrosse St, Pittsburgh, PA, 15218",
+    contact: "(412) 371-0234",
+    iframe: (
+      <iframe
+        title="Echo Health Center"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6074.398742931052!2d-79.883211!3d40.426584000000005!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8834ee7a55e71d0d%3A0x977e71d384f36343!2s1950%20Lacrosse%20St%2C%20Pittsburgh%2C%20PA%2015218%2C%20USA!5e0!3m2!1sen!2sin!4v1653463033564!5m2!1sen!2sin"
+        style={{ border: "0" }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+    ),
+    link: "https://goo.gl/maps/jhk3VJAPqaqwn52N6",
+  },
+  {
+    id: 4,
+    name: "Hawthorne Health Corp.",
+    address: "1069 Forest Creek Dr, Garland, TX, 75043",
+    contact: "(660) 213-3338",
+    iframe: (
+      <iframe
+        title="Hawthorne Health Corp."
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6703.152023388166!2d-96.615877!3d32.856476!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864ea65320475d27%3A0xf4509d7db92083b4!2sDuck%20Creek%20Dr%2C%20Garland%2C%20TX%2075043%2C%20USA!5e0!3m2!1sen!2sin!4v1653463074042!5m2!1sen!2sin"
+        style={{ border: "0" }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+    ),
+    link: "https://goo.gl/mapsf9mu7JdkZjb8qVWZ9",
+  },
+];
