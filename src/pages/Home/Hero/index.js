e8d0ca90bf1fe0531e444f808c98b127248b14fe
Navneet Vishwakarma <navneetk7048@gmail.com>
2022-05-27 16:11:04 +0530

    Added caraosel to Hero component

--- src/pages/Home/Hero/index.js
+++ src/pages/Home/Hero/index.js
@@ -1,45 +1,50 @@
-import React from "react";
-import { useState } from "react";
-import { MdNavigateNext, MdNavigateBefore } from "react-icons/md";
+import Carousel from "react-multi-carousel";
 import hero1 from "../../../assets/images/hero-img1.jpg";
 import hero2 from "../../../assets/images/hero-img2.jpg";
 
 const Hero = () => {
-  const images = [hero1, hero2];
-
-  const [img, setImg] = useState(0);
-
-  const nextImg = () => {
-    if (img + 1 >= images.length) setImg(0);
-    else setImg(img + 1);
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 40000, min: 1000 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1000, min: 576 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 576, min: 0 },
+      items: 1,
+    },
   };
-
-  const prevImg = () => {
-    if (img - 1 < 0) setImg(images.length - 1);
-    else setImg(img - 1);
-  };
-
-  // setInterval(nextImg, 1000);
 
   return (
-    <div className={`hero`}>
-      <img src={images[img]} alt="hero img" />
-      <div className="hero-content">
-        <div className="switch" onClick={prevImg}>
-          <MdNavigateBefore />
-        </div>
-        <div className="hero-text">
-          <div>
+    <div className="hero">
+      <Carousel
+        responsive={responsive}
+        autoPlay={true}
+        infinite={true}
+        autoPlaySpeed={1000}
+        ssr={false}>
+        <div className="hero-img">
+          <img src={hero1} alt="" />
+          <div className="hero-img-text">
             <h1>Getting You Back To Better.</h1>
             <p>
               Have saw replenish saw made bring creature whales darkness evening
             </p>
           </div>
         </div>
-        <div className="switch" onClick={nextImg}>
-          <MdNavigateNext />
+        <div className="hero-img">
+          <img src={hero2} alt="" />
+          <div className="hero-img-text">
+            <h1>Getting You Back To Better.</h1>
+            <p>
+              Have saw replenish saw made bring creature whales darkness evening
+            </p>
+          </div>
         </div>
-      </div>
+      </Carousel>
     </div>
   );
 };
